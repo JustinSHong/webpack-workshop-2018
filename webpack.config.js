e6c703715229ecc68d51ee9d41feeb8afd017af8
JustinSHong <jhong2009@gmail.com>
2019-07-04 18:41:07 -0400

    set up htmlwebpackplugin and webpack.progressplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,26 @@
-module.exports = {
-    output: {
-        filename: "bundle.js"
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const progressOptions = {
+    activeModules: true,
+    entries: false,
+    modules: true,
+    modulesCount: 100,
+    profile: true,
+    handler: (percentage, message, ...args) => {
+        console.info(percentage * 100, message, ...args);
     }
+};
+
+module.exports = env => {
+    return {
+        mode: env.mode,
+        output: {
+            filename: "bundle.js"
+        },
+        plugins: [
+            new HtmlWebpackPlugin(),
+            new webpack.ProgressPlugin(progressOptions)
+        ]
+    };
 };
