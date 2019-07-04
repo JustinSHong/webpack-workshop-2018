1bc2a8ec7b91bc5d66b17c8eb5113a70882ce251
JustinSHong <jhong2009@gmail.com>
2019-07-04 13:34:33 -0400

    change export statements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import nav from "./nav";
 import { top, bottom } from "./footer";
+import createBtn from "./button";
+import { makeColorStyle } from "./button-styles";
 
 console.log("nav", nav());
 console.log("top", top);
 console.log("bottom", bottom);
+console.log("my first button is", createBtn("click here"));
+console.log("color style", makeColorStyle("cyan"));
