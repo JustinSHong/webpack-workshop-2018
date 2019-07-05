fa69f5302b67b08819e8888cbd2e029eb2e39edb
JustinSHong <jhong2009@gmail.com>
2019-07-04 21:27:44 -0400

    set up footer to return a footer node

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import nav from "./nav";
-import { top, bottom } from "./footer";
+import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
 const button = makeButton("first button!");
-document.body.appendChild(button);
+button.style = makeColorStyle("cyan");
 
-console.log(nav(), top, bottom, makeColorStyle("cyan"));
+document.body.appendChild(button);
+document.body.appendChild(footer);
